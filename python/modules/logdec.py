b5534624c419f2f89d40a4fe5af239e4e1b0b03b
ChenlongWang <clwang88@gmail.com>
2017-11-16 22:27:51 +0800

    log decorator

---
+++ python/modules/logdec.py
@@ -0,0 +1,37 @@
+# -*- encoding:utf-8 -*-
+import logging
+
+def log(func):
+    def wrapper(*args, **kw):
+        # create logger
+        logger_name = "example"
+        logger = logging.getLogger(logger_name)
+
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.WARN)
+
+        # create file handler
+        log_path = "./log.log"
+        fh = logging.FileHandler(log_path)
+        fh.setLevel(logging.WARN)
+
+        # create formatter
+        fmt = "%(asctime)-15s %(levelname)s %(filename)s " \
+              "%(lineno)d %(process)d %(message)s"
+        datefmt = "%a %d %b %Y %H:%M:%S"
+        formatter = logging.Formatter(fmt, datefmt)
+
+        fh.setFormatter(formatter)
+        sh.setFormatter(formatter)
+        logger.addHandler(fh)
+        logger.addHandler(sh)
+        logger.warn('warn message %s'%(func.__name__))
+
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def now():
+    print('2015-3-25')
+
+now()
