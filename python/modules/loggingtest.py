9a7ef9806c0ecb9aab502d3072d6de4520ce1a0c
ChenlongWang <clwang88@gmail.com>
2017-11-16 18:40:36 +0800

    add logging

---
+++ python/modules/loggingtest.py
@@ -0,0 +1,28 @@
+# -*- encoding:utf-8 -*-
+import logging
+
+# create logger
+logger_name = "example"
+logger = logging.getLogger(logger_name)
+logger.setLevel(logging.DEBUG)
+
+# create file handler
+log_path = "./log.log"
+fh = logging.FileHandler(log_path)
+fh.setLevel(logging.WARN)
+
+# create formatter
+fmt = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d %(message)s"
+datefmt = "%a %d %b %Y %H:%M:%S"
+formatter = logging.Formatter(fmt, datefmt)
+
+# add handler and formatter to logger
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+# print log info
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+logger.critical('critical message')
