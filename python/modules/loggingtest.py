1e39bed87e9334f66bd8fa133211cee9f43dc3d4
ChenlongWang <clwang88@gmail.com>
2017-11-16 21:33:02 +0800

    both file and stream

--- python/modules/loggingtest.py
+++ python/modules/loggingtest.py
@@ -4,21 +4,26 @@ import logging
 # create logger
 logger_name = "example"
 logger = logging.getLogger(logger_name)
-logger.setLevel(logging.DEBUG)
 
 # create file handler
+sh = logging.StreamHandler()
+sh.setLevel(logging.WARN)
+
 log_path = "./log.log"
 fh = logging.FileHandler(log_path)
 fh.setLevel(logging.WARN)
 
 # create formatter
-fmt = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d %(message)s"
+fmt = "%(asctime)-15s %(levelname)s %(filename)s %(funcName)s %(lineno)d %(" \
+      "process)d %(message)s"
 datefmt = "%a %d %b %Y %H:%M:%S"
 formatter = logging.Formatter(fmt, datefmt)
 
 # add handler and formatter to logger
 fh.setFormatter(formatter)
+sh.setFormatter(formatter)
 logger.addHandler(fh)
+logger.addHandler(sh)
 
 # print log info
 logger.debug('debug message')
